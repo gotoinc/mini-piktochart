e6a4b47c073204f5e8c8533cd2f6838be2701644
gotoinc <root@gotoinc.co>
2022-12-07 20:49:39 +0200

    feature: added images list

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,20 +1,26 @@
 <template>
   <div class="row h-100 w-100 m-0">
-    <SideBar />
-    <CanvasComponent />
+    <SideBar :images="imagesList" />
+    <DrawArea />
+    image list: {{imagesList}}
   </div>
 </template>
 
 <script setup>
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import SideBar from './SideBar.vue'
-import CanvasComponent from './CanvasComponent.vue'
+import DrawArea from './DrawArea.vue'
 
+const imagesList = ref([])
 
 onMounted(async () => {
   try {
-    await fetch('http://localhost:8000/images').then(data => data.json()).then(res => console.log(res))
+    await fetch('http://localhost:8000/images').then(data => data.json()).then(res => {
+      console.log(res, )
+
+      imagesList.value = res
+    })
   } catch(e) {
     console.log('error', e)
   }
